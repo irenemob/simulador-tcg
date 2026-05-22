9c6840ac24d27856db3d67d97e45f57b6907cafc
EQ <irenemothio@eq.ufrj.br>
2026-05-22 16:25:24 -0300

    adicionando loops e formatando

--- simulador_tcg.py
+++ simulador_tcg.py
@@ -1,5 +1,6 @@
 # preparando inputs
 # monstro 1
+
 m1_nome = input('Digite o nome do monstro 1: ').upper()
 m1_vida = int(input(f'Agora, digite os pontos de vida do {m1_nome}: ' ))
 m1_ataque = int(input(f'Por fim, digite os pontos de ataque do {m1_nome}: '))
@@ -13,7 +14,6 @@ if m1_vida <= 0 or m1_ataque <= 0:
 m2_nome = input('\nDigite o nome do monstro 2: ').upper()
 m2_vida = int(input(f'Agora, digite os pontos de vida do {m2_nome}: '))
 m2_ataque = int(input(f'Por fim, digite os pontos de ataque do {m2_nome}: '))
-
 # validacao 2 
 if m2_vida <= 0 or m2_ataque <= 0:
     print('ERRO! A vida e o ataque devem ser maiores que zero. Tente novamente!')
@@ -40,3 +40,27 @@ def exibir_placar(nome1, hp1, nome2, hp2):
     print(f" {nome1}: {hp1} HP")
     print(f" {nome2}: {hp2} HP")
     print("=" * 30 + "\n")
+print("\n    ")
+print( '=' * 30)
+print('Iniciando partida...')
+print( '=' * 30)
+#aplicar loop
+
+while m1_vida > 0  and m2_vida > 0:
+    m2_vida = atacar(m1_nome, m1_ataque, m2_nome, m2_vida)
+    if m2_vida > 0:
+        m1_vida = atacar(m2_nome, m2_ataque, m1_nome, m1_vida)
+    print('\n  ')
+    exibir_placar(m2_nome, m2_vida, m1_nome, m1_vida)
+    
+print( '=' * 30)
+print('      O duelo encerrou!')
+print( '=' * 30)
+print('      RESULTADO DA PARTIDA:')  
+print( '=' * 30)
+
+if m1_vida <= 0:
+    print(f"\nO monstro {m2_nome} é o vencedor do jogo!")
+elif m2_vida <= 0:
+    print(f"\nO monstro {m1_nome} é o vencedor do jogo!")
+
